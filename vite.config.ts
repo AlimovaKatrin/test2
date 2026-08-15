9eaf4602587988034d435d4115bb257258932a27
Алимова Катрин <e.alimova@vk.team>
2026-08-15 12:41:15 +0300

    fix: support StackBlitz mock transport

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,14 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'msw/native': fileURLToPath(
+        new URL('./node_modules/msw/lib/native/index.mjs', import.meta.url),
+      ),
+    },
+  },
+});
